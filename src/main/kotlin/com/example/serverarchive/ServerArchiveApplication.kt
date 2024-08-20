73ed2fbdae5f4d9d795995b80842ea903e358419
pearl <yubicokr@gmail.com>
2024-08-20 21:42:15 +0900

    SetUp for project

---
+++ src/main/kotlin/com/example/serverarchive/ServerArchiveApplication.kt
@@ -0,0 +1,11 @@
+package com.example.serverarchive
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServerArchiveApplication
+
+fun main(args: Array<String>) {
+  runApplication<ServerArchiveApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/serverarchive/ServerArchiveApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.serverarchive
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerArchiveApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
