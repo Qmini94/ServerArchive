8ac721a141da9d5d2f6184a9b04648b41cc6e4a6
박규민 <pkm1614@gmail.com>
2024-08-23 16:06:50 +0900

    간단한 테스트 코드예시 및 코틀린 동작 테스트

--- src/test/kotlin/com/example/serverarchive/ServerArchiveApplicationTests.kt
+++ src/test/kotlin/com/example/serverarchive/ServerArchiveApplicationTests.kt
@@ -1,5 +1,6 @@
 package com.example.serverarchive
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -7,7 +8,13 @@ import org.springframework.boot.test.context.SpringBootTest
 class ServerArchiveApplicationTests {
 
   @Test
-  fun contextLoads() {
+  fun testAddition() {
+    val result = add(2, 3)
+    assertEquals(5, result)
+  }
+
+  private fun add(a: Int, b: Int): Int {
+    return a + b
   }
 
 }
