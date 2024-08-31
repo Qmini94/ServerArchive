5ba0bfa5259cf85ff8b526a5d8af162077aec045
이진주 <l.kylie4630@gmail.com>
2024-08-31 18:15:59 +0900

    feat: YUBI-4-Set User Entity

--- src/main/kotlin/com/example/serverarchive/domain/user/entity/User.kt
+++ src/main/kotlin/com/example/serverarchive/domain/user/entity/User.kt
@@ -1,4 +1,51 @@
 package com.example.serverarchive.domain.user.entity
 
-class User {
-}
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "users")
+data class User(
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  val idx: Int = 0,
+
+  @Column(nullable = false, length = 10)
+  val userName: String,
+
+  @Column(nullable = false, length = 10)
+  val userId: String,
+
+  @Column(nullable = false, length = 255)
+  val password: String,
+
+  @Column(length = 30)
+  val department: String? = null,
+
+  @Column(length = 10)
+  val position: String? = null,
+
+  @Column(length = 20)
+  val email: String? = null,
+
+  @Column(length = 10)
+  val phone: String? = null,
+
+  @Column
+  val level: Int? = null,
+
+  @Column(length = 50)
+  val otp: String? = null,
+
+  @Column
+  val createdDate: LocalDateTime? = null,
+
+  @Column
+  val updatedDate: LocalDateTime? = null,
+
+  @Column(length = 30)
+  val regId: String? = null,
+
+  @Column(columnDefinition = "TEXT")
+  val log: String? = null
+) {}
