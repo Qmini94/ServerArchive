cc77d3740dbdeeb76ba53d7cebcbec04effc88bd
이진주 <l.kylie4630@gmail.com>
2024-08-31 18:02:27 +0900

    feat: YUBI-4-add gradle for security

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,12 +30,16 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+  implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
+  testImplementation("org.springframework.security:spring-security-test")
   compileOnly("org.projectlombok:lombok")
   runtimeOnly("org.postgresql:postgresql")
   annotationProcessor("org.projectlombok:lombok")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+  implementation("org.springframework.boot:spring-boot-starter-web")
 }
 
 kotlin {
