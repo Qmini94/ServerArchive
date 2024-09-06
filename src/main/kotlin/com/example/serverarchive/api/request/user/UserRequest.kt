0f08ac02166c61fb3ae40bcb131ea81995aef025
김성민 <seongmin.work@gmail.com>
2024-09-06 16:26:11 +0900

    fix: [YUBI-8] swagger 설명 추가

--- src/main/kotlin/com/example/serverarchive/api/request/user/UserRequest.kt
+++ src/main/kotlin/com/example/serverarchive/api/request/user/UserRequest.kt
@@ -3,15 +3,23 @@ package com.example.serverarchive.api.request.user
 import com.example.serverarchive.domain.user.entity.User
 import com.example.serverarchive.util.ErrorCodes
 import com.example.serverarchive.util.PasswordUtil
-import java.time.LocalDateTime
+import io.swagger.v3.oas.annotations.media.Schema
 
-class UserRequest(
+@Schema(description = "회원가입 정보")
+data class UserRequest(
+  @Schema(description = "회원명", nullable = false, required = true)
   val userName: String,
+  @Schema(description = "회원ID", nullable = false, required = true)
   val userId: String,
+  @Schema(description = "회원비밀번호", nullable = false, required = true)
   var password: String,
+  @Schema(description = "부서", nullable = true, required = false)
   val department: String? = null,
+  @Schema(description = "직책", nullable = true, required = false)
   val position: String? = null,
+  @Schema(description = "이메일 주소", nullable = true, required = false)
   val email: String? = null,
+  @Schema(description = "연락처", nullable = true, required = false)
   val phone: String? = null,
 ) {
   fun validate(): Boolean {
--- src/main/kotlin/com/example/serverarchive/api/response/user/UserResponse.kt
+++ src/main/kotlin/com/example/serverarchive/api/response/user/UserResponse.kt
@@ -1,19 +1,31 @@
 package com.example.serverarchive.api.response.user
 
 import com.example.serverarchive.domain.user.entity.User
+import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDateTime
 
+@Schema(description = "회원정보")
 data class UserResponse(
   val idx: Int? = null,
+  @Schema(description = "회원명", nullable = false, required = true)
   val userName: String,
+  @Schema(description = "회원ID", nullable = false, required = true)
   val userId: String,
+  @Schema(description = "부서", nullable = true, required = false)
   val department: String? = null,
+  @Schema(description = "직책", nullable = true, required = false)
   val position: String? = null,
+  @Schema(description = "이메일 주소", nullable = true, required = false)
   val email: String? = null,
+  @Schema(description = "연락처", nullable = true, required = false)
   val phone: String? = null,
+  @Schema(description = "회원레벨", nullable = true, required = false)
   val level: Int? = null,
+  @Schema(description = "OTP 인증 Key 값", nullable = true, required = false)
   val otp: String? = null,
+  @Schema(description = "회원가입일", nullable = true, required = false)
   val createdDate: LocalDateTime? = null,
+  @Schema(description = "정보수정일", nullable = true, required = false)
   val updatedDate: LocalDateTime? = null,
   val regId: String? = null,
   val log: String? = null
