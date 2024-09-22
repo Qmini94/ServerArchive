8f6b435f20c623fea675992084fdcebb5528d2d3
이진주 <l.kylie4630@gmail.com>
2024-09-22 15:35:18 +0900

    fix: [YUBI-3] handleUserAjax 함수 수정

--- src/main/resources/static/js/user/ajax.js
+++ src/main/resources/static/js/user/ajax.js
@@ -81,44 +81,41 @@ export const handleUserAjax = () => {
             throw e;
         }
     };
-
-
-    const checkRegisterForm = async (formData) => {
-        try {
-            if (!formData.userId) {
-                await showAlert('Error!', '아이디를 입력해주세요', 'error', $("#inputUserId"));
-                return false;
-            }
-            if (!formData.userName) {
-                await showAlert('Error!', '이름을 입력해주세요', 'error', $("#inputLastName"));
-                return false;
-            }
-            if (!formData.password) {
-                await showAlert('Error!', '비밀번호를 입력해주세요', 'error', $("#inputPassword"));
-                return false;
-            }
-            return true;
-        } catch (e) {
-            console.error(e.message);
-        }
-    };
-
-    const checkLoginForm = async (formData) => {
-        try {
-            if (!formData.userId) {
-                await showAlert('Error!', '아이디를 입력해주세요', 'error', $("#inputUserId"));
-                return false;
-            }
-            if (!formData.password) {
-                await showAlert('Error!', '비밀번호를 입력해주세요', 'error', $("#inputPassword"));
-                return false;
-            }
-            return true;
-        } catch (e) {
-            console.error(e.message);
-        }
-    };
-
-
     return {createUser, loginUser};
+};
+
+const checkRegisterForm = async (formData) => {
+    try {
+        if (!formData.userId) {
+            await showAlert('Error!', '아이디를 입력해주세요', 'error', $("#inputUserId"));
+            return false;
+        }
+        if (!formData.userName) {
+            await showAlert('Error!', '이름을 입력해주세요', 'error', $("#inputLastName"));
+            return false;
+        }
+        if (!formData.password) {
+            await showAlert('Error!', '비밀번호를 입력해주세요', 'error', $("#inputPassword"));
+            return false;
+        }
+        return true;
+    } catch (e) {
+        console.error(e.message);
+    }
+};
+
+const checkLoginForm = async (formData) => {
+    try {
+        if (!formData.userId) {
+            await showAlert('Error!', '아이디를 입력해주세요', 'error', $("#inputUserId"));
+            return false;
+        }
+        if (!formData.password) {
+            await showAlert('Error!', '비밀번호를 입력해주세요', 'error', $("#inputPassword"));
+            return false;
+        }
+        return true;
+    } catch (e) {
+        console.error(e.message);
+    }
 };
