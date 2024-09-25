e0e9fa66a38d067acd1d7dc60643ba4c62759fa3
이진주 <l.kylie4630@gmail.com>
2024-09-25 21:13:44 +0900

    fix: [YUBI-4] 로그인. 회원가입 html 수정

--- src/main/resources/static/js/user/write.js
+++ src/main/resources/static/js/user/write.js
@@ -1,32 +1,49 @@
 import { formatPhoneNumber } from '/js/common.js';
 import { handleUserAjax } from '/js/user/ajax.js';
 
-$(function () {
-    const userAjax = handleUserAjax();
+const userAjax = handleUserAjax();
 
-    $("#inputUserId").on('input', function () {
-        const checkBtn = $('.check-userId-btn');
-        checkBtn.removeClass('btn-success').addClass('btn-light');
-        checkBtn.data('checked', false);
-        checkBtn.text('중복확인');
-    });
-    
-    $(".check-userId-btn").on('click', function () {
-        userAjax.checkDuplicateUid();
-    });
+export const handleUserRegisterForm = () => {
+    try {
+        $("#inputUserId").on('input', function () {
+            const checkBtn = $('.check-userId-btn');
+            checkBtn.removeClass('btn-success').addClass('btn-light');
+            checkBtn.data('checked', false);
+            checkBtn.text('중복확인');
+        });
 
-    $(".btn-register").on('click', function (e) {
-        e.preventDefault();
-        $('#inputEmail').val(getFullEmail());
-        userAjax.createUser();
-    });
+        $(".check-userId-btn").on('click', function () {
+            userAjax.checkDuplicateUid();
+        });
 
-    $("#inputPhone").on('input', function (e) {
-        formatPhoneNumber(e.target);
-    });
+        $(".btn-register").on('click', function (e) {
+            e.preventDefault();
+            $('#inputEmail').val(getFullEmail());
+            userAjax.createUser();
+        });
 
-    $('#emailDomainSelect').on('change', toggleCustomDomainInput);
-});
+        $("#inputPhone").on('input', function (e) {
+            formatPhoneNumber(e.target);
+        });
+
+        $('#emailDomainSelect').on('change', toggleCustomDomainInput);
+    } catch (e) {
+        console.error(e.message);
+        throw e;
+    }
+};
+
+export const handleUserLoginForm = () => {
+    try {
+        $(".btn-login").on('click', function (e) {
+            e.preventDefault();
+            userAjax.loginUser();
+        });
+    } catch (e) {
+        console.error(e.message);
+        throw e;
+    }
+};
 
 const toggleCustomDomainInput = () => {
     try {
