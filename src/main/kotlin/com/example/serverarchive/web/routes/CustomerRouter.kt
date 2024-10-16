9ababcffb65f50d2e030364ea21c9ee32c8da5c3
박규민 <pkm1614@gmail.com>
2024-10-16 09:03:22 +0900

    Revert "feat: [YUBI-11] PaginationUtil 생성 및 업체목록 페이징 처리"
    This reverts commit 41a716453b04a6662b62d559ec66c05a43c7a0cd.

--- src/main/kotlin/com/example/serverarchive/web/routes/CustomerRouter.kt
+++ src/main/kotlin/com/example/serverarchive/web/routes/CustomerRouter.kt
@@ -2,6 +2,7 @@ package com.example.serverarchive.web.routes
 
 import com.example.serverarchive.api.response.customer.CustomerCreateResponse
 import com.example.serverarchive.service.customer.CustomerService
+import com.example.serverarchive.util.PaginationUtil
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType
@@ -17,6 +18,7 @@ class CustomerRouter(private val customerService: CustomerService) {
         ("/customer").nest {
             GET("/create", ::viewCreatePage)
             GET("/update/{idx}", ::viewUpdatePage)
+            GET("/list", ::viewListPage)
         }
     }
 
@@ -38,5 +40,22 @@ class CustomerRouter(private val customerService: CustomerService) {
             "customer" to customer
         )
         return ServerResponse.ok().contentType(MediaType.TEXT_HTML).render("client/customer/create", data)
+    }
+
+    fun viewListPage(req: ServerRequest): ServerResponse  {
+        val pageable = PaginationUtil.parseParams(req)
+        val customers = customerService.getCustomerList(pageable)
+        val startIndex = (customers.number * customers.size) + 1
+
+        val data = mapOf(
+            "pageTitle" to "업체 목록",
+            "customers" to customers.content,
+            "currentPage" to (customers.number + 1),
+            "totalElements" to customers.totalElements,
+            "size" to customers.size,
+            "startIndex" to startIndex
+        )
+
+        return ServerResponse.ok().contentType(MediaType.TEXT_HTML).render("client/customer/list", data)
     }
 }
